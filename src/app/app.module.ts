7eeeee5646ed9f417f1dfd7e5fab57bb8e8279a3
Abdur Rehman <abdurrehman418@gmail.com>
2022-10-14 11:45:30 +0500

    basic architecture setups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
+import { ForgotPasswordComponent } from './project/auth/forgot-password/forgot-password.component';
+import { LoginComponent } from './project/auth/login/login.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './project/canvas/canvas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent,
+    LoginComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
